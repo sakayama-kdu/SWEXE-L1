efabc860fee0947ac4dc82ed414a838eebb3d7d4
2411140074 <2411140074m@ed.fuk.kindai.ac.jp>
2026-09-18 03:24:17 +0000

    基礎課題

---
+++ db/migrate/20260918031123_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[8.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.integer :price
+      t.date :published_on
+
+      t.timestamps
+    end
+  end
+end
